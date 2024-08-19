3acbf924898035448aff332fecc5a7f386e8bb36
hadishamoun1 <shamounhadi2@gmail.com>
2024-08-19 22:15:25 +0300

    Added port to .env

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
