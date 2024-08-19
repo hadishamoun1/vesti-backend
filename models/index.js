5eaa2db8b29513eae35417919cf02371b15001e9
hadishamoun1 <shamounhadi2@gmail.com>
2024-08-19 21:13:18 +0300

    Added relations for models

---
+++ models/index.js
@@ -0,0 +1,55 @@
+const sequelize = require('../connection/connection');
+const User = require('./User');
+const Store = require('./Store');
+const Product = require('./Product');
+const Order = require('./Order');
+const OrderItem = require('./OrderItem');
+const Notification = require('./Notification');
+const Discount = require('./Discount');
+const StoreCategory = require('./StoreCategory');
+
+
+User.hasMany(Store, { foreignKey: 'owner_id' });
+Store.belongsTo(User, { foreignKey: 'owner_id' });
+
+Store.hasMany(Product, { foreignKey: 'store_id' });
+Product.belongsTo(Store, { foreignKey: 'store_id' });
+
+User.hasMany(Order, { foreignKey: 'user_id' });
+Order.belongsTo(User, { foreignKey: 'user_id' });
+
+Store.hasMany(Order, { foreignKey: 'store_id' });
+Order.belongsTo(Store, { foreignKey: 'store_id' });
+
+Order.hasMany(OrderItem, { foreignKey: 'order_id' });
+OrderItem.belongsTo(Order, { foreignKey: 'order_id' });
+
+Product.hasMany(OrderItem, { foreignKey: 'product_id' });
+OrderItem.belongsTo(Product, { foreignKey: 'product_id' });
+
+User.hasMany(Notification, { foreignKey: 'user_id' });
+Notification.belongsTo(User, { foreignKey: 'user_id' });
+
+Store.hasMany(Notification, { foreignKey: 'store_id' });
+Notification.belongsTo(Store, { foreignKey: 'store_id' });
+
+Store.hasMany(Discount, { foreignKey: 'store_id' });
+Discount.belongsTo(Store, { foreignKey: 'store_id' });
+
+Product.hasMany(Discount, { foreignKey: 'product_id' });
+Discount.belongsTo(Product, { foreignKey: 'product_id' });
+
+Store.hasMany(StoreCategory, { foreignKey: 'store_id' });
+StoreCategory.belongsTo(Store, { foreignKey: 'store_id' });
+
+module.exports = {
+  User,
+  Store,
+  Product,
+  Order,
+  OrderItem,
+  Notification,
+  Discount,
+  StoreCategory,
+  sequelize
+};
