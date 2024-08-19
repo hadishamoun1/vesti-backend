b1c77644d9f412aad92a3290514bfab833af95df
hadishamoun1 <shamounhadi2@gmail.com>
2024-08-19 20:57:47 +0300

    Added products model

---
+++ models/Products.js
@@ -0,0 +1,52 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../connection/connection'); // Adjust path as needed
+const Store = require('./Store');
+
+const Product = sequelize.define('Product', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  storeId: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: Store,
+      key: 'id'
+    }
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  description: {
+    type: DataTypes.TEXT
+  },
+  price: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false
+  },
+  category: {
+    type: DataTypes.STRING,
+    defaultValue: 'shoes'
+  },
+  imageUrl: {
+    type: DataTypes.TEXT
+  },
+  discount: {
+    type: DataTypes.DECIMAL(5, 2)
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  }
+}, {
+  tableName: 'Products',
+  timestamps: false
+});
+
+module.exports = Product;
