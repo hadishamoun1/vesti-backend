e8e9f4685746c39b6aaa6745759f477b3068d6de
hadishamoun1 <shamounhadi2@gmail.com>
2024-08-19 20:50:05 +0300

    Added users model

---
+++ models/Users.js
@@ -0,0 +1,42 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../connection/connection'); // Adjust path as needed
+
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  phoneNumber: {
+    type: DataTypes.STRING
+  },
+  location: {
+    type: DataTypes.GEOMETRY('POINT')
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  }
+}, {
+  tableName: 'users',
+  timestamps: false
+});
+
+module.exports = User;
