07ae80d97b3f0483a23c652a1eb71a59ebe6a333
hadishamoun1 <shamounhadi2@gmail.com>
2024-08-26 18:12:20 +0300

    Added route to get nearby stores based on users location

--- routes/storeRoutes.js
+++ routes/storeRoutes.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { Store } = require('../models/index');
 const WebSocket = require('ws');
+const { Op } = require('sequelize');
+const { Sequelize } = require('sequelize');
+
+const RADIUS_IN_METERS = 5000; // Define the radius for nearby stores (e.g., 5 km)
 
 // Create a new store
 router.post('/', async (req, res) => {
@@ -22,16 +26,37 @@ router.post('/', async (req, res) => {
   }
 });
 
-
 // Get all stores with optional limit
 router.get('/', async (req, res) => {
   try {
-    // Get the limit from the query parameters
     const limit = parseInt(req.query.limit, 10) || null; // Default to null if not provided
-    
-    // Fetch stores with or without limit
     const stores = limit ? await Store.findAll({ limit }) : await Store.findAll();
-    
+    res.json(stores);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// Get nearby stores based on user's location
+router.get('/nearby', async (req, res) => {
+  try {
+    const { lat, lon, limit } = req.query;
+
+    if (!lat || !lon) {
+      return res.status(400).json({ error: 'Latitude and longitude are required' });
+    }
+
+    const stores = await Store.findAll({
+      where: Sequelize.where(
+        Sequelize.fn('ST_Distance',
+          Sequelize.col('location'),
+          Sequelize.fn('ST_MakePoint', lon, lat)
+        ),
+        { [Op.lt]: RADIUS_IN_METERS }
+      ),
+      limit: parseInt(limit, 10) || 10 // Default to 10 stores if not provided
+    });
+
     res.json(stores);
   } catch (error) {
     res.status(400).json({ error: error.message });
