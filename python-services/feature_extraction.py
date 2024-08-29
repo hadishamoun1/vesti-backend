10752fbf62bad5d6340eac822ca3d9f2286849dc
hadishamoun1 <shamounhadi2@gmail.com>
2024-08-29 18:31:32 +0300

    Updated extract features and removed 140 pictures from uploads

--- python-services/feature_extraction.py
+++ python-services/feature_extraction.py
@@ -1,10 +1,11 @@
 import tensorflow as tf
 from tensorflow.keras.applications import VGG16
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.vgg16 import preprocess_input, decode_predictions
+from tensorflow.keras.applications.vgg16 import preprocess_input
 import numpy as np
+import os
+import json
 
-# Load pre-trained model
 model = VGG16(weights='imagenet', include_top=False, pooling='avg')
 
 def extract_features(img_path):
@@ -12,6 +13,29 @@ def extract_features(img_path):
     img_array = image.img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0)
     img_array = preprocess_input(img_array)
-
     features = model.predict(img_array)
-    return features.tolist() 
+    return features.flatten().tolist() 
+
+def extract_features_from_folder(folder_path):
+    features_list = []
+    filenames = []
+    for filename in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, filename)
+        if os.path.isfile(file_path):
+            features = extract_features(file_path)
+            features_list.append(features)
+            filenames.append(filename)
+    return features_list, filenames
+
+
+UPLOAD_FOLDER = 'uploads'
+TEMP_FOLDER = 'temp'
+
+upload_features, upload_filenames = extract_features_from_folder(UPLOAD_FOLDER)
+temp_features, temp_filenames = extract_features_from_folder(TEMP_FOLDER)
+
+with open('upload_features.json', 'w') as f:
+    json.dump({'features': upload_features, 'filenames': upload_filenames}, f)
+
+with open('temp_features.json', 'w') as f:
+    json.dump({'features': temp_features, 'filenames': temp_filenames}, f)
