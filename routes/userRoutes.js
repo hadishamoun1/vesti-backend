642d7c72cffaf0c463c9a1c07b912f2a77a6cf03
hadishamoun1 <shamounhadi2@gmail.com>
2024-09-05 16:16:12 +0300

    Fixed missing import bcrypt

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { User } = require("../models/index");
 const sequelize = require('../connection/connection');
+const bcrypt = require('bcrypt'); 
 
 // Create a new user
 router.post("/", async (req, res) => {
