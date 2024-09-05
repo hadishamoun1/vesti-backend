46eb31d7d04b6b4757387d7727b3c9ad5bd2c28f
hadishamoun1 <shamounhadi2@gmail.com>
2024-09-05 16:38:37 +0300

    Edited user model removed location

--- models/User.js
+++ models/User.js
@@ -1,42 +1,44 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../connection/connection');
+const { DataTypes } = require("sequelize");
+const sequelize = require("../connection/connection");
 
-const User = sequelize.define('User', {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true
+const User = sequelize.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    phoneNumber: {
+      type: DataTypes.STRING,
+    },
+
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
   },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  email: {
-    type: DataTypes.STRING,
-    unique: true,
-    allowNull: false
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  phoneNumber: {
-    type: DataTypes.STRING
-  },
-  location: {
-    type: DataTypes.GEOMETRY('POINT')
-  },
-  createdAt: {
-    type: DataTypes.DATE,
-    defaultValue: DataTypes.NOW
-  },
-  updatedAt: {
-    type: DataTypes.DATE,
-    defaultValue: DataTypes.NOW
+  {
+    tableName: "users",
+    timestamps: false,
   }
-}, {
-  tableName: 'users',
-  timestamps: false
-});
+);
 
 module.exports = User;
